eefd3c6099a5a6a72b99d704faa0bfc5365c985c
shushant <shushantkumar0625@gmail.com>
2025-04-13 05:00:49 +0530

    url changes

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
   }, [])
 
   async function reviewCode() {
-    const response = await axios.post('http://localhost:3000/ai/get-review', { code })
+    const response = await axios.post('https://code-review-backend-62xo.onrender.com', { code })
     setReview(response.data)
   }
 
